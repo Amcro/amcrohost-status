ccba0c607032e854704a3b18c809a1ff9d6e961e
Amcro <alan4salic@gmail.com>
2023-04-30 23:51:36 +0200

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -40,13 +40,9 @@ do
     sleep 5
   done
   dateTime=$(date +'%Y-%m-%d %H:%M')
-  if [[ $commit == true ]]
-  then
+  
     echo $dateTime, $result >> "logs/${key}_report.log"
     # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
     echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
-  else
-    echo "    $dateTime, $result"
-  fi
 done
 
