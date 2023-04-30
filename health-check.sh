2b16624ba8afd61ba8c2f912ca464408975bb188
Amcro <alan4salic@gmail.com>
2023-05-01 00:04:30 +0200

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -1,6 +1,9 @@
-# In the original repository we'll just print the result of status checks,
-# without committing. This avoids generating several commits that would make
-# later upstream merges messy for anyone who forked us.
+commit=true
+origin=$(git remote get-url origin)
+if [[ $origin == *statsig-io/statuspage* ]]
+then
+  commit=false
+fi
 
 KEYSARRAY=()
 URLSARRAY=()
@@ -34,11 +37,28 @@ do
     else
       result="failed"
     fi
+    if [ "$result" = "success" ]; then
+      break
+    fi
+    sleep 5
   done
   dateTime=$(date +'%Y-%m-%d %H:%M')
-  
+  if [[ $commit == true ]]
+  then
     echo $dateTime, $result >> "logs/${key}_report.log"
     # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
     echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
+  else
+    echo "    $dateTime, $result"
+  fi
 done
 
+if [[ $commit == true ]]
+then
+  # Let's make Vijaye the most productive person on GitHub.
+  git config --global user.name 'Vijaye Raji'
+  git config --global user.email 'vijaye@statsig.com'
+  git add -A --force logs/
+  git commit -am '[Automated] Update Health Check Logs'
+  git push
+fi
